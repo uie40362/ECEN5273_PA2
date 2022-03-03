404c71c22cb172c86aabf52aa7a842ef053fe888
uie40362 <uie40362@contiwan.com>
2022-03-02 23:02:55 -0700

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_2.dir/main.c.o"
+  "Assignment_2.pdb"
+  "Assignment_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/httpserv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/httpserv.dir/www/httpechosrv.c.o"
+  "httpserv.pdb"
+  "httpserv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/httpserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
